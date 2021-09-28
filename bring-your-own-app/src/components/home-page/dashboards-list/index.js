0983e9e316f78d04ad06fb6f6fd1292c499cc65b
Renee Casaus <shawncasaus@Shawns-MBP.lan1>
2021-09-27 23:06:58 -0700

    fix: missed a file

--- bring-your-own-app/src/components/home-page/dashboards-list/index.js
+++ bring-your-own-app/src/components/home-page/dashboards-list/index.js
@@ -1,31 +1,9 @@
-import React, {useState, useEffect}  from 'react';
-import axios from 'axios';
+import React from 'react';
 import DashboardsTable from './dashboards-table';
+import { useHomePage } from '../context';
 
 const DashboardsList = () => {
-    const [definitions, setDefinitions] = useState([]);
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(true);
-
-    //calls backend to get past used urls
-    useEffect(() => {    
-        const fetchUrl = 'http://localhost:5000/api/dashboards'; //used for backend call
-
-        //preset axios options
-        const options = {
-            method: 'GET',
-            mode: 'no-cors'
-        };
-
-        axios(fetchUrl, options).then(response => {
-                setDefinitions(response.data);
-            }).catch (err => {
-                console.error('Error: ', err);
-                setError(true);
-            }).finally(() => {
-                setLoading(false);
-            });
-    }, []);
+    const { definitions, loading, error } = useHomePage();
 
     //handle loading and errors
     if (loading) return 'loading...';
